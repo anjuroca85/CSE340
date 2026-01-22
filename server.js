74df2abbf71d4fedd84fa698b7e749f1f664b34b
anjuroca85 <anjuroca85@gmail.com>
2026-01-22 16:07:31 -0500

    Handling basic errors in week 3

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ const static = require("./routes/static");
 const baseController = require("./controllers/baseController");
 // Inventory routes
 const inventoryRoute = require("./routes/inventoryRoute");//This seems to have been missed by the course training
+const utilities = require("./utilities");
+
 
 
 /* ***********************
@@ -33,6 +35,25 @@ app.use(static);
 app.get("/", baseController.buildHome);
 // Inventory routes
 app.use("/inv", inventoryRoute);
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
+})
 
 /* ***********************
  * Local Server Information
